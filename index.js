9c5a29310e856a4f1f16cd8ea9bd5c472d8ed0fb
boisterous-array-6571 <malika.luck@gmail.com>
2019-09-28 03:17:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+logShout =(string) =>{
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  var lowercase = string.toLowerCase();
+  if (string.toLowerCase() === lowercase){
+    return 'I can\'t hear you!'
+  } 
+}
+function sayHiToGrandma(string) {
+  var uppercase = string.toUpperCase();
+  if (string.toUpperCase() === uppercase) {
+  sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  return 'YES INDEED!';
+  } 
+} 
